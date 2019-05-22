ade76148ec028401eed225adc815e23aaaf278d6
willys_tjiawi <willys@huubap.com>
2019-05-23 00:35:22 +0800

    refactor LOGIN (with hashed password)

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,21 +15,32 @@ exports.getAuth = (req, res, next) => {
 exports.postAuth = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+    let currentUser;
 
     User.findOne({email: email})
     .then(user => {
         console.log('postAuth_user..... ', user);
+        currentUser = user;
 
-        if(!user || password !== user.password) {
+        if(!user) {
             return res.redirect('/login');
         }
 
-        req.session.isLoggedIn = true;     // set a property in session
-        req.session.user = user;
-        req.session.save(err => {   // save the above set session before redirect just as guarantee the session is set before redirecting
-            console.log(err);
-            res.redirect('/');
-        });
+        return bcrypt.compare(password, user.password); // compare the input password & hashed password in DB (one way encryption)
+    })
+    .then(passwordMatch => {
+        console.log('postAuth_passwordMatch..... ', passwordMatch);
+
+        if(passwordMatch) {
+            req.session.isLoggedIn = true;     // set a property in session
+            req.session.user = currentUser;
+            req.session.save(err => {   // save the above set session before redirect just as guarantee the session is set before redirecting
+                console.log(err);
+                res.redirect('/');
+            });
+        } else {
+            res.redirect('/login');
+        }
     })
     .catch(err => {console.log(err)});
 };
